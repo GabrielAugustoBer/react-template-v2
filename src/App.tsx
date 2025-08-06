663da8caa09f5c0c9980df4fc0c1da21ef3bc423
Gabriel Augusto <92814110+GabrielAugustoBer@users.noreply.github.com>
2025-08-05 21:16:02 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export function App() {
     return (
         <div>
-<h1 className="">Hello World</h1>
-        <h2></h2>
+          <h1 className="">Hello World</h1>
+        <h2>this is a template for projects</h2>
         </div>
         
     )
